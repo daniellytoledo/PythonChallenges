1f2962825534dea55d6bf97fadcac56f586961ba
Dani <daniellytoledosouto@gmail.com>
2024-12-08 16:40:30 +0100

    alterando retorno da api do Flask

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello there! I happy you're here to see how I'm doing with my projects.</p>"
